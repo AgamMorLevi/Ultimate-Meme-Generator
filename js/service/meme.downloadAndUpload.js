46f6bf297b586e4aab9dc14fc7301faf8d0e7672
Agam Mor-Levi <agammor96@gmail.com>
2024-12-25 15:45:19 +0200

    Addind shere on facebook option

--- js/service/meme.downloadAndUpload.js
+++ js/service/meme.downloadAndUpload.js
@@ -42,3 +42,39 @@ function renderImg(img) {
 
   renderLines()
 }
+
+function onUploadImg(ev) {
+  ev.preventDefault()
+  const canvasData = gElCanvas.toDataURL('image/jpeg')
+
+  // After a succesful upload, allow the user to share on Facebook
+  function onSuccess(uploadedImgUrl) {
+    const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+    console.log('encodedUploadedImgUrl:', encodedUploadedImgUrl)
+    document.querySelector('.share-option').innerHTML = `
+    
+        <button class="btn-facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}')">
+           Share on Facebook  
+        </button>`
+  }
+  uploadImg(canvasData, onSuccess)
+}
+
+async function uploadImg(imgData, onSuccess) {
+  const CLOUD_NAME = 'webify'
+  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+  const formData = new FormData()
+  formData.append('file', imgData)
+  formData.append('upload_preset', 'webify')
+  try {
+    const res = await fetch(UPLOAD_URL, {
+      method: 'POST',
+      body: formData,
+    })
+    const data = await res.json()
+    console.log('Cloudinary response:', data)
+    onSuccess(data.secure_url)
+  } catch (err) {
+    console.log(err)
+  }
+}
