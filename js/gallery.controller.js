29ff5be56d60a5852f3d4e099d2b97cc645e8a07
Agam Mor-Levi <agammor96@gmail.com>
2024-12-27 19:03:28 +0200

    finish ui for categories-container

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -87,3 +87,45 @@ function filterImages(searchTerm, category) {
 
   renderGallery(filteredImgs)
 }
+
+const categoryData = {
+  funny: { popularity: 1, fontSize: 16 },
+  money: { popularity: 1, fontSize: 16 },
+  cute: { popularity: 1, fontSize: 16 },
+  stoned: { popularity: 1, fontSize: 16 },
+}
+
+function onOptionClick(category) {
+  categoryData[category].popularity++
+  const categoryElement = document.querySelector(`[data-value="${category}"]`)
+  const selectElement = document.querySelector('.category-select')
+  selectElement.selectedIndex = 0
+  if (categoryElement) {
+    let newFontSize
+    const popularity = categoryData[category].popularity
+
+    if (window.innerWidth < 768) {
+      newFontSize = 16 + popularity * 2
+      newFontSize = Math.min(newFontSize, 30)
+      newFontSize = Math.max(newFontSize, 16)
+    } else {
+      newFontSize = 28 + popularity * 2
+      newFontSize = Math.min(newFontSize, 60)
+      newFontSize = Math.max(newFontSize, 28)
+    }
+
+    categoryData[category].fontSize = newFontSize
+    categoryElement.style.fontSize = `${newFontSize}px`
+
+    for (let otherCategory in categoryData) {
+      if (otherCategory !== category) {
+        let otherCategoryElement = document.querySelector(`[data-value="${otherCategory}"]`)
+        let otherFontSize = categoryData[otherCategory].fontSize
+
+        otherFontSize = Math.max(otherFontSize - 2, window.innerWidth < 768 ? 16 : 28)
+        categoryData[otherCategory].fontSize = otherFontSize
+        otherCategoryElement.style.fontSize = `${otherFontSize}px`
+      }
+    }
+  }
+}
